633e5e9dfb65db864b886cfc9e1fb099f3d98345
prontospecs <prontospecs@gmail.com>
2026-03-12 10:55:59 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,63 +1,72 @@
-// --- 1. НАШ КЛЮЧ MAPILLARY И МЕГА-БАЗА ГОРОДОВ ---
+// --- 1. НАШ КЛЮЧ MAPILLARY И РАСШИРЕННАЯ БАЗА ГОРОДОВ ---
 const myAccessToken = 'MLY|26266369579645993|cf11c635af876bc5f1a671e0f6401897';
 
-// Расширенная база: более 100 городов для мгновенной загрузки!
 const CITIES = [
-    // 🏰 Европа и СНГ
+    // 🏰 ЕВРОПА (Только Европа и часть СНГ)
     { name: "Лондон", lat: 51.5074, lon: -0.1278, region: "europe" }, { name: "Париж", lat: 48.8566, lon: 2.3522, region: "europe" },
     { name: "Рим", lat: 41.9028, lon: 12.4964, region: "europe" }, { name: "Берлин", lat: 52.5200, lon: 13.4050, region: "europe" },
     { name: "Мадрид", lat: 40.4168, lon: -3.7038, region: "europe" }, { name: "Амстердам", lat: 52.3676, lon: 4.9041, region: "europe" },
     { name: "Вена", lat: 48.2082, lon: 16.3738, region: "europe" }, { name: "Прага", lat: 50.0755, lon: 14.4378, region: "europe" },
     { name: "Варшава", lat: 52.2297, lon: 21.0122, region: "europe" }, { name: "Будапешт", lat: 47.4979, lon: 19.0402, region: "europe" },
     { name: "Стокгольм", lat: 59.3293, lon: 18.0686, region: "europe" }, { name: "Афины", lat: 37.9838, lon: 23.7275, region: "europe" },
-    { name: "Стамбул", lat: 41.0082, lon: 28.9784, region: "europe" }, { name: "Москва", lat: 55.7558, lon: 37.6173, region: "europe" },
-    { name: "Санкт-Петербург", lat: 59.9343, lon: 30.3351, region: "europe" }, { name: "Новосибирск", lat: 55.0084, lon: 82.9357, region: "europe" },
+    { name: "Москва", lat: 55.7558, lon: 37.6173, region: "europe" }, { name: "Санкт-Петербург", lat: 59.9343, lon: 30.3351, region: "europe" },
     { name: "Екатеринбург", lat: 56.8389, lon: 60.6057, region: "europe" }, { name: "Казань", lat: 55.7963, lon: 49.1088, region: "europe" },
-    { name: "Нижний Новгород", lat: 56.3269, lon: 44.0059, region: "europe" }, { name: "Астана", lat: 51.1694, lon: 71.4491, region: "europe" },
-    { name: "Алматы", lat: 43.2220, lon: 76.8512, region: "europe" }, { name: "Минск", lat: 53.9006, lon: 27.5590, region: "europe" },
-    { name: "Барселона", lat: 41.3851, lon: 2.1734, region: "europe" }, { name: "Милан", lat: 45.4642, lon: 9.1900, region: "europe" },
-    { name: "Мюнхен", lat: 48.1351, lon: 11.5820, region: "europe" }, { name: "Киев", lat: 50.4501, lon: 30.5234, region: "europe" },
-    { name: "Ташкент", lat: 41.2995, lon: 69.2401, region: "europe" }, { name: "Баку", lat: 40.4093, lon: 49.8671, region: "europe" },
-    { name: "Тбилиси", lat: 41.7151, lon: 44.8271, region: "europe" },
+    { name: "Минск", lat: 53.9006, lon: 27.5590, region: "europe" }, { name: "Барселона", lat: 41.3851, lon: 2.1734, region: "europe" }, 
+    { name: "Милан", lat: 45.4642, lon: 9.1900, region: "europe" }, { name: "Мюнхен", lat: 48.1351, lon: 11.5820, region: "europe" }, 
+    { name: "Киев", lat: 50.4501, lon: 30.5234, region: "europe" }, { name: "Осло", lat: 59.9139, lon: 10.7522, region: "europe" },
+    { name: "Хельсинки", lat: 60.1695, lon: 24.9354, region: "europe" }, { name: "Дублин", lat: 53.3498, lon: -6.2603, region: "europe" },
+    { name: "Лиссабон", lat: 38.7223, lon: -9.1393, region: "europe" },
 
-    // 🌎 Америка (Северная и Южная)
-    { name: "Нью-Йорк", lat: 40.7128, lon: -74.0060, region: "americas" }, { name: "Лос-Анджелес", lat: 34.0522, lon: -118.2437, region: "americas" },
-    { name: "Чикаго", lat: 41.8781, lon: -87.6298, region: "americas" }, { name: "Торонто", lat: 43.6510, lon: -79.3470, region: "americas" },
-    { name: "Мехико", lat: 19.4326, lon: -99.1332, region: "americas" }, { name: "Сан-Франциско", lat: 37.7749, lon: -122.4194, region: "americas" },
-    { name: "Сиэтл", lat: 47.6062, lon: -122.3321, region: "americas" }, { name: "Вашингтон", lat: 38.9072, lon: -77.0369, region: "americas" },
-    { name: "Сан-Паулу", lat: -23.5505, lon: -46.6333, region: "americas" }, { name: "Рио-де-Жанейро", lat: -22.9068, lon: -43.1729, region: "americas" },
-    { name: "Буэнос-Айрес", lat: -34.6037, lon: -58.3816, region: "americas" }, { name: "Богота", lat: 4.7110, lon: -74.0721, region: "americas" },
-    { name: "Лима", lat: -12.0464, lon: -77.0428, region: "americas" }, { name: "Сантьяго", lat: -33.4489, lon: -70.6693, region: "americas" },
-    { name: "Ванкувер", lat: 49.2827, lon: -123.1207, region: "americas" }, { name: "Монреаль", lat: 45.5017, lon: -73.5673, region: "americas" },
-    { name: "Майами", lat: 25.7617, lon: -80.1918, region: "americas" }, { name: "Хьюстон", lat: 29.7604, lon: -95.3698, region: "americas" },
-    { name: "Бостон", lat: 42.3601, lon: -71.0589, region: "americas" }, { name: "Гвадалахара", lat: 20.6597, lon: -103.3496, region: "americas" },
-    { name: "Медельин", lat: 6.2442, lon: -75.5812, region: "americas" }, { name: "Бразилиа", lat: -15.8267, lon: -47.9218, region: "americas" },
-    { name: "Монтевидео", lat: -34.9011, lon: -56.1645, region: "americas" },
-
-    // 🏮 Азия и Ближний Восток
+    // 🏮 АЗИЯ (Азия, Ближний Восток, часть СНГ)
     { name: "Токио", lat: 35.6762, lon: 139.6503, region: "asia" }, { name: "Осака", lat: 34.6937, lon: 135.5023, region: "asia" },
     { name: "Сеул", lat: 37.5665, lon: 126.9780, region: "asia" }, { name: "Сингапур", lat: 1.3521, lon: 103.8198, region: "asia" },
     { name: "Гонконг", lat: 22.3193, lon: 114.1694, region: "asia" }, { name: "Бангкок", lat: 13.7563, lon: 100.5018, region: "asia" },
     { name: "Пекин", lat: 39.9042, lon: 116.4074, region: "asia" }, { name: "Шанхай", lat: 31.2304, lon: 121.4737, region: "asia" },
     { name: "Куала-Лумпур", lat: 3.1390, lon: 101.6869, region: "asia" }, { name: "Джакарта", lat: -6.2088, lon: 106.8456, region: "asia" },
     { name: "Манила", lat: 14.5995, lon: 120.9842, region: "asia" }, { name: "Дели", lat: 28.6139, lon: 77.2090, region: "asia" },
-    { name: "Мумбаи", lat: 19.0760, lon: 72.8777, region: "asia" }, { name: "Бангалор", lat: 12.9716, lon: 77.5946, region: "asia" },
-    { name: "Дубай", lat: 25.2048, lon: 55.2708, region: "asia" }, { name: "Эр-Рияд", lat: 24.7136, lon: 46.6753, region: "asia" },
-    { name: "Тель-Авив", lat: 32.0853, lon: 34.7818, region: "asia" }, { name: "Тайбэй", lat: 25.0330, lon: 121.5654, region: "asia" },
-    { name: "Киото", lat: 35.0116, lon: 135.7681, region: "asia" }, { name: "Хошимин", lat: 10.8231, lon: 106.6297, region: "asia" },
-    { name: "Дакка", lat: 23.8103, lon: 90.4125, region: "asia" }, { name: "Карачи", lat: 24.8607, lon: 67.0011, region: "asia" },
-    { name: "Доха", lat: 25.2854, lon: 51.5310, region: "asia" },
+    { name: "Мумбаи", lat: 19.0760, lon: 72.8777, region: "asia" }, { name: "Дубай", lat: 25.2048, lon: 55.2708, region: "asia" }, 
+    { name: "Эр-Рияд", lat: 24.7136, lon: 46.6753, region: "asia" }, { name: "Тель-Авив", lat: 32.0853, lon: 34.7818, region: "asia" }, 
+    { name: "Тайбэй", lat: 25.0330, lon: 121.5654, region: "asia" }, { name: "Ташкент", lat: 41.2995, lon: 69.2401, region: "asia" }, 
+    { name: "Астана", lat: 51.1694, lon: 71.4491, region: "asia" }, { name: "Алматы", lat: 43.2220, lon: 76.8512, region: "asia" },
+    { name: "Новосибирск", lat: 55.0084, lon: 82.9357, region: "asia" }, { name: "Стамбул", lat: 41.0082, lon: 28.9784, region: "asia" },
+    { name: "Баку", lat: 40.4093, lon: 49.8671, region: "asia" }, { name: "Тбилиси", lat: 41.7151, lon: 44.8271, region: "asia" },
 
-    // 🦁 Африка и Океания
+    // 🗽 СЕВЕРНАЯ АМЕРИКА
+    { name: "Нью-Йорк", lat: 40.7128, lon: -74.0060, region: "north_america" }, { name: "Лос-Анджелес", lat: 34.0522, lon: -118.2437, region: "north_america" },
+    { name: "Чикаго", lat: 41.8781, lon: -87.6298, region: "north_america" }, { name: "Торонто", lat: 43.6510, lon: -79.3470, region: "north_america" },
+    { name: "Мехико", lat: 19.4326, lon: -99.1332, region: "north_america" }, { name: "Сан-Франциско", lat: 37.7749, lon: -122.4194, region: "north_america" },
+    { name: "Сиэтл", lat: 47.6062, lon: -122.3321, region: "north_america" }, { name: "Вашингтон", lat: 38.9072, lon: -77.0369, region: "north_america" },
+    { name: "Ванкувер", lat: 49.2827, lon: -123.1207, region: "north_america" }, { name: "Монреаль", lat: 45.5017, lon: -73.5673, region: "north_america" },
+    { name: "Майами", lat: 25.7617, lon: -80.1918, region: "north_america" }, { name: "Хьюстон", lat: 29.7604, lon: -95.3698, region: "north_america" },
+    { name: "Бостон", lat: 42.3601, lon: -71.0589, region: "north_america" }, { name: "Гвадалахара", lat: 20.6597, lon: -103.3496, region: "north_america" },
+    { name: "Даллас", lat: 32.7767, lon: -96.7970, region: "north_america" }, { name: "Атланта", lat: 33.7490, lon: -84.3880, region: "north_america" },
+    { name: "Денвер", lat: 39.7392, lon: -104.9903, region: "north_america" }, { name: "Монтеррей", lat: 25.6866, lon: -100.3161, region: "north_america" },
+
+    // 🌴 ЮЖНАЯ АМЕРИКА
+    { name: "Сан-Паулу", lat: -23.5505, lon: -46.6333, region: "south_america" }, { name: "Рио-де-Жанейро", lat: -22.9068, lon: -43.1729, region: "south_america" },
+    { name: "Буэнос-Айрес", lat: -34.6037, lon: -58.3816, region: "south_america" }, { name: "Богота", lat: 4.7110, lon: -74.0721, region: "south_america" },
+    { name: "Лима", lat: -12.0464, lon: -77.0428, region: "south_america" }, { name: "Сантьяго", lat: -33.4489, lon: -70.6693, region: "south_america" },
+    { name: "Медельин", lat: 6.2442, lon: -75.5812, region: "south_america" }, { name: "Бразилиа", lat: -15.8267, lon: -47.9218, region: "south_america" },
+    { name: "Монтевидео", lat: -34.9011, lon: -56.1645, region: "south_america" }, { name: "Кито", lat: -0.1807, lon: -78.4678, region: "south_america" },
+    { name: "Каракас", lat: 10.4806, lon: -66.9036, region: "south_america" }, { name: "Белу-Оризонти", lat: -19.9167, lon: -43.9345, region: "south_america" },
+    { name: "Куритиба", lat: -25.4284, lon: -49.2733, region: "south_america" },
+
+    // 🦁 АФРИКА
     { name: "Каир", lat: 30.0444, lon: 31.2357, region: "africa" }, { name: "Кейптаун", lat: -33.9249, lon: 18.4241, region: "africa" },
-    { name: "Йоханнесбург", lat: -26.2041, lon: 28.0473, region: "africa" }, { name: "Сидней", lat: -33.8688, lon: 151.2093, region: "africa" },
-    { name: "Мельбурн", lat: -37.8136, lon: 144.9631, region: "africa" }, { name: "Окленд", lat: -36.8485, lon: 174.7633, region: "africa" },
-    { name: "Претория", lat: -25.7479, lon: 28.2293, region: "africa" }, { name: "Дурбан", lat: -29.8587, lon: 31.0218, region: "africa" },
-    { name: "Найроби", lat: -1.2921, lon: 36.8219, region: "africa" }, { name: "Аккра", lat: 5.6037, lon: -0.1870, region: "africa" },
-    { name: "Лагос", lat: 6.5244, lon: 3.3792, region: "africa" }, { name: "Дакар", lat: 14.7167, lon: -17.4677, region: "africa" },
-    { name: "Брисбен", lat: -27.4705, lon: 153.0260, region: "africa" }, { name: "Перт", lat: -31.9505, lon: 115.8605, region: "africa" },
-    { name: "Аделаида", lat: -34.9285, lon: 138.6007, region: "africa" }, { name: "Веллингтон", lat: -41.2865, lon: 174.7762, region: "africa" },
-    { name: "Крайстчерч", lat: -43.5321, lon: 172.6362, region: "africa" }
+    { name: "Йоханнесбург", lat: -26.2041, lon: 28.0473, region: "africa" }, { name: "Претория", lat: -25.7479, lon: 28.2293, region: "africa" }, 
+    { name: "Дурбан", lat: -29.8587, lon: 31.0218, region: "africa" }, { name: "Найроби", lat: -1.2921, lon: 36.8219, region: "africa" }, 
+    { name: "Аккра", lat: 5.6037, lon: -0.1870, region: "africa" }, { name: "Лагос", lat: 6.5244, lon: 3.3792, region: "africa" }, 
+    { name: "Дакар", lat: 14.7167, lon: -17.4677, region: "africa" }, { name: "Касабланка", lat: 33.5731, lon: -7.5898, region: "africa" },
+    { name: "Тунис", lat: 36.8065, lon: 10.1815, region: "africa" }, { name: "Алжир", lat: 36.7538, lon: 3.0588, region: "africa" },
+    { name: "Луанда", lat: -8.8390, lon: 13.2894, region: "africa" },
+
+    // 🦘 АВСТРАЛИЯ И ОКЕАНИЯ
+    { name: "Сидней", lat: -33.8688, lon: 151.2093, region: "oceania" }, { name: "Мельбурн", lat: -37.8136, lon: 144.9631, region: "oceania" }, 
+    { name: "Окленд", lat: -36.8485, lon: 174.7633, region: "oceania" }, { name: "Брисбен", lat: -27.4705, lon: 153.0260, region: "oceania" }, 
+    { name: "Перт", lat: -31.9505, lon: 115.8605, region: "oceania" }, { name: "Аделаида", lat: -34.9285, lon: 138.6007, region: "oceania" }, 
+    { name: "Веллингтон", lat: -41.2865, lon: 174.7762, region: "oceania" }, { name: "Крайстчерч", lat: -43.5321, lon: 172.6362, region: "oceania" },
+    { name: "Голд-Кост", lat: -28.0167, lon: 153.4000, region: "oceania" }, { name: "Хобарт", lat: -42.8821, lon: 147.3272, region: "oceania" },
+    { name: "Дарвин", lat: -12.4634, lon: 130.8456, region: "oceania" }, { name: "Гамильтон", lat: -37.7870, lon: 175.2793, region: "oceania" }
 ];
 
 // Глобальные настройки игры
@@ -69,7 +78,7 @@ let gameSettings = {
 async function getRandomImageId() {
     const cleanToken = myAccessToken.trim(); 
     
-    // Фильтруем города по региону
+    // Фильтруем города по строгому региону
     let availableCities = CITIES;
     if (gameSettings.region !== 'all') {
         availableCities = CITIES.filter(c => c.region === gameSettings.region);
